587320b0a004ae226a793acf64f66ad710ccb7c6
Jared Chesebro <jaredchesebro@novuslabs.com>
2023-02-07 12:27:45 -0800

    attempt to render a chess board

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,34 @@
+use std::fmt;
+
+const SIZE: usize = 8;
+
+struct Square<'a> {
+    color: &'a str,
+}
+
+impl<'a> fmt::Display for Square<'a> {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "{}", self.color)
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let mut grid = [[Square { color: "" }; SIZE]; SIZE];
+
+    for i in 0..SIZE {
+        for j in 0..SIZE {
+            grid[i][j].color = if (i + j) % 2 == 0 {
+                "black"
+            } else {
+                "white"
+            };
+        }
+    }
+
+    for row in grid.iter() {
+        for square in row.iter() {
+            print!("{} ", square);
+        }
+        println!("");
+    }
 }
